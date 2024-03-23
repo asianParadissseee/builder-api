0ee0749ecdb4000bc2451b9dd06800b5f1dc47b0
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-23 20:42:19 +0600

    create articles

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { NewsModule } from "./news/news.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [MongooseModule.forRootAsync({
     useFactory: () => ({
       uri:process.env["MONGO_URI"]
     })
-  }), NewsModule],
+  }), NewsModule, ArticlesModule],
   controllers: [AppController],
   providers: [AppService]
 })
