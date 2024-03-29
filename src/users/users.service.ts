f648441b479c4eea4e70893c3dcc544571c11ac6
asianParadissseee <asian.paradissseee@gmail.com>
2024-03-29 12:21:48 +0600

    add feature for modue to users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private usersModel: Model<User>) {
+
   }
 
-  findAll() {
-    return `This action returns all users`;
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      return await new this.usersModel(createUserDto).save();
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException(`Ошибка создания юзера ${e}`);
+    }
+  }
+
+
+  async findAll() {
+    try {
+      return await this.usersModel.find();
+    } catch (e) {
+      throw new InternalServerErrorException(`Ошибка получение всех юзеров ${e}`);
+    }
   }
 
   findOne(id: number) {
